e16e5a7232ff0d4ff846f90f56233859573662fb
Prasoon Soni <prasoonsoni03@gmail.com>
2022-12-01 11:31:09 +0530

    Initial Setup Done

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const app = express()
+const PORT = process.env.PORT || 5000
+dotenv.config()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send("App Running.")
+})
+app.listen(PORT, () => {
+    console.log(`App running at ${process.env.BASE_URL}`)
+})
