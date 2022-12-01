11aac1f6a8df159be5921b8055d9fe7be88c8624
Prasoon Soni <prasoonsoni03@gmail.com>
2022-12-01 11:44:40 +0530

    Added Database Connection

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const app = express()
+const connectToDatabase = require('./database/connection')
 const PORT = process.env.PORT || 5000
+connectToDatabase()
 dotenv.config()
 app.use(cors())
 app.use(express.json())
@@ -11,5 +13,6 @@ app.get('/', (req, res)=>{
     res.send("App Running.")
 })
 app.listen(PORT, () => {
-    console.log(`App running at ${process.env.BASE_URL}`)
+    console.log(`App running at ${process.env.BASE_URL} and using port ${PORT}`)
+
 })
