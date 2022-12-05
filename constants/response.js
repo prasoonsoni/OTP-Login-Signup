a211b863a5821024b8013b597de06f8913f9ee7e
Prasoon Soni <prasoonsoni03@gmail.com>
2022-12-05 10:57:21 +0530

    Added response constants

---
+++ constants/response.js
@@ -0,0 +1,5 @@
+const SERVER_ERROR = "Internal Server Error Occured"
+const OTP_SUCCESS = "OTP sent successfully"
+const OTP_ERROR = "Error sending OTP"
+
+module.exports = { SERVER_ERROR, OTP_ERROR, OTP_SUCCESS }
--- controllers/otpController.js
+++ controllers/otpController.js
@@ -2,6 +2,7 @@ const OTP = require('../models/OTP')
 const sendSms = require('../scripts/sendSms')
 const sendEmail = require('../scripts/sendEmail')
 const generateOTP = require('../scripts/generateOTP')
+const { SERVER_ERROR, OTP_SUCCESS, OTP_ERROR } = require('../constants/response')
 
 const sendOtp = async (req, res) => {
     try {
@@ -11,16 +12,16 @@ const sendOtp = async (req, res) => {
             if (type === "phone") {
                 const sms = await sendSms(user, otpPresent.otp)
                 if (sms.body) {
-                    return res.json({ success: true, message: "OTP Sent Successfully" })
+                    return res.json({ success: true, message: OTP_SUCCESS })
                 }
             }
             if (type === "email") {
                 const email = await sendEmail(user, otpPresent.otp)
                 if (email.response) {
-                    return res.json({ success: true, message: "OTP Sent Successfully" })
+                    return res.json({ success: true, message: OTP_SUCCESS })
                 }
             }
-            return res.json({ success: false, message: "Error sending OTP" })
+            return res.json({ success: false, message: OTP_ERROR })
         }
         const otp = await OTP.create({
             user: user,
@@ -28,24 +29,24 @@ const sendOtp = async (req, res) => {
             otp: generateOTP(4)
         })
         if (!otp) {
-            res.json({ sucess: false, message: "Error sending OTP" })
+            res.json({ sucess: false, message: OTP_ERROR })
         }
         if (type === "phone") {
             const sms = await sendSms(user, otp.otp)
             if (!sms.body) {
-                return res.json({ success: true, message: "Error Sending OTP" })
+                return res.json({ success: true, message: OTP_ERROR })
             }
         }
         if (type === "email") {
             const email = await sendEmail(user, otp.otp)
             if (!email.response) {
-                return res.json({ success: true, message: "Error Sending OTP" })
+                return res.json({ success: true, message: OTP_ERROR })
             }
         }
-        res.json({ success: true, message: "OTP Sent Successfully" })
+        res.json({ success: true, message: OTP_SUCCESS })
     } catch (error) {
         console.log(error.message)
-        res.json({ success: false, message: "Internal Server Error Occured" })
+        res.json({ success: false, message: SERVER_ERROR })
     }
 }
 
