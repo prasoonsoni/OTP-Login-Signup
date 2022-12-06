8c900a530061841b1a592ba764ee710020a4e9ef
Prasoon Soni <prasoonsoni03@gmail.com>
2022-12-06 15:53:42 +0530

    Added User Model

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('user', UserSchema)
